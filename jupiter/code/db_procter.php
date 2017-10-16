f153fdf43fab615206ca02f312f8e6b6a66a5645
soyborias <rmartinez@avansitsolutions.com>
2017-10-16 15:22:43 -0500

    Setup DB

--- jupiter/code/db_procter.php
+++ jupiter/code/db_procter.php
@@ -1,8 +1,5 @@
 <?php
-  defined('DB_HOST') || define('DB_HOST','');
-  defined('DB_USER') || define('DB_USER','');
-  defined('DB_PASSWORD') || define('DB_PASSWORD','');
-  defined('DB_NAME') || define('DB_NAME','');
+  include_once('../../config.php');
   
   include_once('MysqliDb.php');
 	$mysqli = new mysqli (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
